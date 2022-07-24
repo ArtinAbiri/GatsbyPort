75f0386b16395c666a8ad74022e22e15ff365808
Artin Abiri <kevinabiri@gmail.com>
2022-07-24 21:30:03 +0200

    metadata

--- src/components/SEO/SEO.jsx
+++ src/components/SEO/SEO.jsx
@@ -13,10 +13,12 @@ export const SEO = ({ title, description, pathname, children }) => {
             <meta name="description" content={seo.description} />
             <meta name="LinkedIn:card" content="summary_large_image" />
             <meta name="LinkedIn:description" content={seo.description} />
-            <link rel="apple-touch-icon" href="icons/img192.png"></link>
+            <link rel="apple-touch-icon" href="/icons/icon-192x192.png"/>
             <meta name="keywords" content="JavaScript, Artin, Artin Abiri, Abiri, Management, IT, ReactJS, Chalmers, Developer, Portfolio, IT Portfolio"/>
             <meta name="author" content="Artin Abiri"/>
             <meta name="theme-color" content="#f5f0ec"/>
+            <link rel="manifest" href="/manifest.json" />
+
             {children}
         </>
     )
